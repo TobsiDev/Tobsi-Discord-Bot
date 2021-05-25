633336db45dde2be8d44584dc90ff71b4bc99e32
Tobsi <>
2021-05-25 12:50:21 +0000

    Fixed something...

--- commands/test.js
+++ commands/test.js
@@ -9,12 +9,11 @@ module.exports = {
             .setAuthor('TobsiDev')
             .setDescription('Test embed.')
             .addFields(
-                { name: 'CummieRule 1', value: 'Dont CUM in me plz.' },
-                { name: 'CummieRule 2', value: 'Dont CUM on my GitHub plz.' },
-                { name: 'CummieRule 3', value: 'Dont CUM in my mouth plz.' }
+                { name: 'Field 1', value: 'Check out my repo.' },
+                { name: 'Field 2', value: 'Another one.' }
             )
             .setImage('https://images.emojiterra.com/google/android-11/512px/1f525.png')
-            .setFooter('Plz, dont CUM in me. I dont like CUMMIES in my tummy.');
+            .setFooter('I cant do a lot.');
 
         message.channel.send(`Hello. Im ${client.user.tag}`);
         message.channel.send(newEmbed);
