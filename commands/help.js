57ef68052f9e152e973260455fbf7d8a9bbd7e17
Tobsi <tsh40884137@gmail.com>
2021-06-06 23:16:58 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,27 @@
+const Discord = require('discord.js');
+
 module.exports = {
     name: 'help',
     description: "this is a command that says doot...",
-    execute(message, args) {
-        message.channel.send("``` ====== ADMIN ====== \n something \n  ====== ADMIN ====== ```");
-        console.log('msg: ' + message);
-        console.log('arg: ' + args);
+    execute(message, args, Discord, client) {
+			
+			const newEmbed = new Discord.MessageEmbed()
+			.setColor('#1f618d')
+			.addFields(
+				{ name: '**Admin Commands**', value: 'All the admin commands:'},
+				{ name: '** - Set your role(s)**', value: 'Add a role to you: Use !set-role'},
+				{ name: '** - Remove your role(s)**', value: 'Remove a role: Use !rm-role'},
+				{ name: '** - Kick someone**', value: 'Kick: Use !kick @User'},
+				{ name: '**Normal Commands**', value: 'All the normal commands everyone can use:'},
+				{ name: '** - YouTube**', value: 'Watch YouTube with your friends: Use !youtube / !yt / !vid'},
+				{ name: '** - South Park Quote**', value: 'Get a random Quote from the tv show, South Park: Use !southpark / !sp / !SOUTHPARK'},
+				{ name: '**TODO Commands**', value: 'NOT IMPLEMENTET YET:'}
+			)
+			.setFooter('I cant do a lot.');
+
+			message.channel.send(newEmbed);
+
+        /*console.log('msg: ' + message);
+        console.log('arg: ' + args);*/
     }
 }
